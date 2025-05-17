b5bbaa12621bf5ec3660d4e0f3fd26895089d149
Suyash Mishra <143060685+SuyashMishr@users.noreply.github.com>
2025-05-17 14:15:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     data.append('file', file);
 
     try {
-      const res = await axios.post('http://localhost:8000/upload', data, {
+      const res = await axios.post('https://filesharing-backend-khaki.vercel.app/upload', data, {
         onUploadProgress: (progressEvent) => {
           const percent = Math.round((progressEvent.loaded * 100) / progressEvent.total);
           setUploadProgress(percent);
